3afb24347b22f90c2971b372a888d5564d50f297
TOMOKI O <64746406+tomokio@users.noreply.github.com>
2021-05-15 14:42:48 +0900

    Added translation_ja_JP.ts.

---
+++ translations/translation_ja_JP.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+    <context>
+        <name>behavior.xar:/MAIN/Animated Say</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>わかりましたか</source>
+            <comment>Text</comment>
+            <translation type="unfinished">わかりましたか</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/MAIN/Animated Say (1)</name>
+        <message>
+            <source>おわりだよ</source>
+            <comment>Text</comment>
+            <translation type="obsolete">おわりだよ</translation>
+        </message>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>おめでとう。カボチャが完成したね。お疲れさま。今日のワークショップは終了だよ。</source>
+            <comment>Text</comment>
+            <translation type="unfinished">おめでとう。カボチャが完成したね。お疲れさま。今日のワークショップは終了だよ。</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/MAIN/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+</TS>
